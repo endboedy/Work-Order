52ec093dd1c62f2e9651d6de2bfdd8c26a720d1a
endboedy <ndarboe.net@outlook.com>
2025-08-28 14:07:30 +0800

    Update script.js

--- Menu3_Detail_Part_and_Order/script.js
+++ Menu3_Detail_Part_and_Order/script.js
@@ -1,1 +1,19 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  Promise.all([
+    fetch("../../data_sources/Data1.json").then(res => res.json()),
+    fetch("../../data_sources/Data2.json").then(res => res.json())
+  ])
+  .then(([data1, data2]) => {
+    const table = document.getElementById("detailTable");
+    [...data1, ...data2].forEach(item => {
+      const row = table.insertRow();
+      Object.values(item).forEach(val => {
+        const cell = row.insertCell();
+        cell.textContent = val;
+      });
+    });
+  })
+  .catch(err => console.error("Error loading detail data:", err));
+});
+
